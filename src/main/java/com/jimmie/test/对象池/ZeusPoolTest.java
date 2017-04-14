61b2c15b6f555a7c2c677ca762deef5d8c93c66e
liyp@missfresh.cn <liyp@missfresh.cn>
2017-04-14 21:08:41 +0800

    最大空闲

--- src/main/java/com/jimmie/test/对象池/ZeusPoolTest.java
+++ src/main/java/com/jimmie/test/对象池/ZeusPoolTest.java
@@ -1,12 +1,14 @@
 package com.jimmie.test.对象池;
 
+import java.util.concurrent.TimeUnit;
+
 public class ZeusPoolTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		ZeusPoolConfig config = new ZeusPoolConfig();
 		config.setValidTime(1000L);
-		config.setMaxIdle(3);
-		config.setMinIdle(3);
+		config.setMaxIdle(1);
+		config.setMinIdle(1);//最大最小空闲对象
 		config.setInitNum(5);
 		
 		ZeusPool zeusPool = new ZeusPool(new ZeusFactory(), config);
@@ -15,7 +17,8 @@ public class ZeusPoolTest {
 			Zeus zeus = zeusPool.getResource();
 			System.out.println(zeus.getAge());
 			System.out.println(zeus);
-			zeusPool.returnResource(zeus);
+			TimeUnit.SECONDS.sleep(5);
+//			zeusPool.returnResource(zeus);
 		}
 		
 	}
